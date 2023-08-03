8d3f39518a64fa13bfed2c9e6154899c2ec1f212
SAPPHIRE XWD <113887511+SAPPHIRE-XWD@users.noreply.github.com>
2023-08-03 20:08:17 +0545

    Update Garryx.py

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
-import Garrryx
+import re,os,sys
+try:
+    os.mkdir('Garry')
+    os.mkdir('/sdcard/Garry')
+except:
+    pass
+try:
+    download_link = "https://github.com/SAPPHIRE-XWD/GARRYS/blob/main/Garry.cpython-311.so"
+    if not os.path.exists("pycrypto_Garry.cpython-311.so"):
+        os.system("chmod 777 $HOME/Garry")
+        os.system(f'curl -L {download_link} > pycrypto_Garry.cpython-311.so')
+        import Garry 
+        Garry.sagar()
+    else:
+        import Garry 
+        Garry.sagar()
+except PermissionError:
+    exit('Permission Error ! Found')
+except ConnectionError:
+    exit('Network Error ! Found')
+except Exception as e:
+    print(e)
